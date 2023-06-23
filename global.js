2d8e58458ae8dea17fc2d175c4c736fde0c27e4a
Molly <dudumikah@gmail.com>
2023-06-23 14:25:57 +0500

    About process.env process.argv

--- global.js
+++ global.js
@@ -1,4 +1,11 @@
-const http = require('http');
+// console.log(global) // on console write Node global
 
+// setTimeout (() => {
+//     console.log("Hello World")
+// }, 2000)
 
-console.log(http.createServer())
+// 1.console.log(__dirname);
+
+// console.log(global.queueMicrotask())
+
+console.log(process.argv)
